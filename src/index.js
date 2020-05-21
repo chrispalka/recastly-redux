48c6d5d90006538ac440358a6d3d980f30a461fe
Chris Palka <cpalka87gmail.com>
2020-05-20 21:43:13 -0600

    Complete refactor of searchYouTube via fetch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './components/App.js';
-import searchYouTube from './lib/searchYouTube.js';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 import handleSearchChange from './actions/search.js';
+import VideoListContainer from './containers/VideoListContainer';
+// import Root from './components/Root.js';
+
 //TODO: Import the Provider component from 'react-redux' here!
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router>
+//       <App API_KEY={YOUTUBE_API_KEY} searchYouTube={handleSearchChange} />
+//     </Router>
+//   </Provider>,
+//   document.getElementById('app')
+// );
+
 ReactDOM.render(
   <Provider store={store}>
-    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={handleSearchChange} />
+    <Router>
+      <Route exact path="/home" component={App} API_KEY={YOUTUBE_API_KEY} searchYouTube={handleSearchChange} />
+      <Route path="/videolist" component={VideoListContainer} />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
-
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,22 +1,54 @@
+// var searchYouTube = ({key, query, max = 5}, callback) => {
+//   $.get('https://www.googleapis.com/youtube/v3/search', {
+//     part: 'snippet',
+//     key: key,
+//     q: query,
+//     maxResults: max,
+//     type: 'video',
+//     videoEmbeddable: 'true'
+//   })
+//     .done(({items}) => {
+//       if (callback) {
+//         callback(items);
+//       }
+//     })
+//     .fail(({responseJSON}) => {
+//       responseJSON.error.errors.forEach((err) =>
+//         console.error(err)
+//       );
+//     });
+// };
+
+
 var searchYouTube = ({key, query, max = 5}, callback) => {
-  $.get('https://www.googleapis.com/youtube/v3/search', {
+  const url = 'https://www.googleapis.com/youtube/v3/search/?';
+  fetch(url + new URLSearchParams({
     part: 'snippet',
     key: key,
     q: query,
     maxResults: max,
     type: 'video',
     videoEmbeddable: 'true'
-  })
-    .done(({items}) => {
+  }))
+    .then( response => response.json())
+    .then( data => {
+      console.log('Success:', data);
       if (callback) {
-        callback(items);
+        callback(data.items);
       }
     })
-    .fail(({responseJSON}) => {
-      responseJSON.error.errors.forEach((err) =>
-        console.error(err)
-      );
-    });
+    .catch((error) => console.error(error));
 };
 
+
 export default searchYouTube;
+
+
+/*
+.catch((error) => {
+      console.error('Error:', error);
+    });
+*/
+
+
+
